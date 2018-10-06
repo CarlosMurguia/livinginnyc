edc1fc34968754c62b8ba907d1b8ddc2c4180fe1
Carlos Murguia <carlos@creativecycle.io>
2018-10-05 23:22:55 -0400

    apps

--- app.py
+++ app.py
@@ -246,6 +246,13 @@ while True:
 def read_csv():
     return page
 
+@app.after_request 
+def after_request(response):
+    header = response.headers
+    header['Access-Control-Allow-Origin'] = '*'
+    return response
+
+
 if __name__ == "__main__":
     port = int(os.environ.get('PORT', 5000))
     app.run(host='127.0.0.1', port=5000)
